dc1fb0fb1634aebb3164c9d642b452b7704183b1
Andwele Ancheta <andwele.ancheta@gmail.com>
2024-09-18 18:43:16 +1000

    Change base

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-  createHashRouter,
-  RouterProvider
+  BrowserRouter as Router,
+  Routes,
+  Route
 } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Services from './pages/Services.jsx';
@@ -17,51 +18,27 @@ import Hobbies from './pages/Hobbies.jsx';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <Home />
-  },
-  {
-    path: "services",
-    element: <Services />
-  },
-  {
-    path: "childcare",
-    element: <Childcare />
-  },
-  {
-    path: "forms",
-    element: <Forms />
-  },
-  {
-    path: "about",
-    element: <About />
-  },
-  {
-    path: "enrol",
-    element: <Enrol />
-  },
-  {
-    path: "literacy",
-    element: <Literacy />
-  },
-  {
-    path: "computer",
-    element: <Computer />
-  },
-  {
-    path: "community",
-    element: <Community />
-  },
-  {
-    path: "hobbies",
-    element: <Hobbies />
-  }
-]);
+const basename = '/LangwarrinCC-Deploy';
+
+const App = () => (
+  <Router basename={basename}>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="services" element={<Services />} />
+      <Route path="childcare" element={<Childcare />} />
+      <Route path="forms" element={<Forms />} />
+      <Route path="about" element={<About />} />
+      <Route path="enrol" element={<Enrol />} />
+      <Route path="literacy" element={<Literacy />} />
+      <Route path="computer" element={<Computer />} />
+      <Route path="community" element={<Community />} />
+      <Route path="hobbies" element={<Hobbies />} />
+    </Routes>
+  </Router>
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
