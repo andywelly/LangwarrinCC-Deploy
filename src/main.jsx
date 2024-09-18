89ca0beceffab2b5e6e950556226c3deb833ff38
Andwele Ancheta <andwele.ancheta@gmail.com>
2024-09-18 18:29:28 +1000

    Test HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,70 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Home from './pages/Home.jsx'
-// import Services from './pages/Services.jsx'
-// import Childcare from './pages/Childcare.jsx'
-// import Forms from './pages/Forms.jsx'
-// import About from './pages/About.jsx'
-// import Computer from './pages/Computer.jsx'
-// import Enrol from './pages/Enrol.jsx'
-// import Literacy from './pages/Literacy.jsx'
-// import Community from './pages/Community.jsx'
-// import Hobbies from './pages/Hobbies.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Home from './pages/Home.jsx';
+import Services from './pages/Services.jsx';
+import Childcare from './pages/Childcare.jsx';
+import Forms from './pages/Forms.jsx';
+import About from './pages/About.jsx';
+import Computer from './pages/Computer.jsx';
+import Enrol from './pages/Enrol.jsx';
+import Literacy from './pages/Literacy.jsx';
+import Community from './pages/Community.jsx';
+import Hobbies from './pages/Hobbies.jsx';
 
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-// } from "react-router-dom";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Home/>
-//   },
-//   {
-//     path: "services",
-//     element: <Services/>
-//   },
-//   {
-//     path: "childcare",
-//     element: <Childcare/>
-//   },
-//   {
-//     path: "forms",
-//     element: <Forms/>
-//   },
-//   {
-//     path: "about",
-//     element: <About/>
-//   },
-//   { 
-//     path: "enrol",
-//     element: <Enrol/>
-//   },
-//   { 
-//     path: "literacy",
-//     element: <Literacy/>
-//   },
-//   { 
-//     path: "computer",
-//     element: <Computer/>
-//   },
-//   { 
-//     path: "community",
-//     element: <Community/>
-//   },
-//   { 
-//     path: "hobbies",
-//     element: <Hobbies/>
-//   },
-
-// ]);
-
+import {
+  HashRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home/>
-  </StrictMode>,
-)
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="services" element={<Services />} />
+        <Route path="childcare" element={<Childcare />} />
+        <Route path="forms" element={<Forms />} />
+        <Route path="about" element={<About />} />
+        <Route path="enrol" element={<Enrol />} />
+        <Route path="literacy" element={<Literacy />} />
+        <Route path="computer" element={<Computer />} />
+        <Route path="community" element={<Community />} />
+        <Route path="hobbies" element={<Hobbies />} />
+      </Routes>
+    </HashRouter>
+  </StrictMode>
+);
